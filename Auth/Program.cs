6f681817aa7ea387eaffc8579508002f19612564
danielschoder <daniel@schoder.uk>
2024-10-08 19:25:54 +0100

    V10-95

--- Auth/Program.cs
+++ Auth/Program.cs
@@ -2,6 +2,8 @@ using Auth.DAL;
 using Auth.ApiEndpoints;
 using Auth.Data;
 using Microsoft.EntityFrameworkCore;
+using Auth.BLL;
+using Auth.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IUserAccessor, UserAccessor>();
-
 #endregion
+
+builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+builder.Services.AddScoped<IUserManager, UserManager>();
+builder.Services.AddScoped<TokenProvider, TokenProvider>();
 
 builder.Services.AddCors();
 
